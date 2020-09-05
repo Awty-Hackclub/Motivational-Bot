daaaa7b2c74f7f63cfbb337078446cd534df74cb
Tarek Ali <tarekali15@outlook.com>
2020-09-05 14:26:07 -0500

    finished the MVP

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
 from discord.ext import commands
 import yaml
+import random
 
 with open('config.yaml') as file:
     data = yaml.load(file, Loader=yaml.FullLoader)
-    token = data['token']
 
 bot = commands.Bot(command_prefix='!')
 
 @bot.command()
 async def motivation(ctx):
-    ...
+    await ctx.send(random.choice(open('quotes.txt').readlines()))
 
-bot.run(token)
+bot.run(data['token'])
 
 
