d2a3c196b9b8f12170d45668353787c0adb37fdc
Tarek Ali <tarekali15@outlook.com>
2020-09-05 14:34:45 -0500

    rich embed for motivation command

--- main.py
+++ main.py
@@ -11,6 +11,16 @@ bot = commands.Bot(command_prefix='!')
 
 @bot.command()
 async def motivation(ctx):
-    await ctx.send(random.choice(open('quotes.txt').readlines()))
+    embed = discord.Embed(
+        title='Motivational Quote',
+        description=random.choice(open('quotes.txt').readlines()),
+        colour=discord.Colour.red()
+    )
+
+    message = await client.say(embed=embed)
+
+    await bot.add_reaction(message, '\U+1F44D')
+    await bot.add_reaction(message, '\U+1F49D')
+    await bot.add_reaction(message, '\U+1F917')
 
 bot.run(data['token'])
